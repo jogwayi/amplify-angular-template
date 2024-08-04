8d49f497fe7ce0a872204a2faecf8582c9003218
Josephat <jogwayi@users.noreply.github.com>
2024-08-04 18:48:18 +0300

    Custom datasource

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,24 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-defineBackend({
+import { aws_dynamodb } from "aws-cdk-lib";
+
+export const backend = defineBackend({
   auth,
   data,
 });
+
+const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
+
+
+const externalTable = aws_dynamodb.Table.fromTableName(
+  externalDataSourcesStack,
+  "MyExternalPostTable",
+  "PostTable"
+);
+
+
+backend.data.addDynamoDbDataSource(
+  "ExternalPostTableDataSource",
+  externalTable
+);
