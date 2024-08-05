3e68e6eba51c7b71f0a442097432501a1d2337de
Josephat <jogwayi@users.noreply.github.com>
2024-08-06 00:04:01 +0300

    Add files via upload

--- amplify/data/batchGetPost.js
+++ amplify/data/batchGetPost.js
@@ -3,21 +3,11 @@ import { util } from '@aws-appsync/utils/lib';
 
 export const request = function(ctx) {
   const { partition_key, sort_key } = ctx.args
-  var keys = []
-  sort_key.forEach((sk) => {
-    keys.push(util.dynamodb.toMapValues({ product_number_sku: sk, base_store: partition_key }));
-  });
-    //var key = sort_key.map(sk => util.dynamodb.toMapValues({ product_number_sku: sk, base_store: partition_key }))
+  
     return {
       operation: "BatchGetItem",
       tables: {
-        PostTable: keys
-         /* {
-          //keys: sort_key.map(sk => util.dynamodb.toMapValues({ product_number_sku: sk, base_store: partition_key })),
-          // keys: sort_key.map(sk => util.dynamodb.toMapValues({ product_number_sku: sk, base_store: partition_key })),
-          key: util.dynamodb.toMapValues({ product_number_sku: sort_key[0], base_store: partition_key }),
-          consistentRead: true,
-        } */
+        PostTable: [util.dynamodb.toMapValues({ base_store: partition_key })]
       },
     };
 }
