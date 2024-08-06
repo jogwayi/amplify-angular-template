a885d6592ecf77889df4af61b56557e9a5f04e06
Josephat <jogwayi@users.noreply.github.com>
2024-08-06 15:59:46 +0300

    Add files via upload

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,24 +2,7 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-import { aws_dynamodb } from "aws-cdk-lib";
-
 export const backend = defineBackend({
   auth,
   data,
 });
-
-const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
-
-
-const externalTable = aws_dynamodb.Table.fromTableName(
-  externalDataSourcesStack,
-  "MyExternalPostTable",
-  "PostTable"
-);
-
-
-backend.data.addDynamoDbDataSource(
-  "ExternalPostTableDataSource",
-  externalTable
-);
