5ae0d405da0d8089188fdd68659341e666607a18
Josephat <jogwayi@users.noreply.github.com>
2024-08-06 11:05:14 +0300

    Add files via upload

--- amplify/data/batchGetPost.js
+++ amplify/data/batchGetPost.js
@@ -2,12 +2,12 @@ import { util } from '@aws-appsync/utils/lib';
 
 
 export const request = function(ctx) {
-  const { base_store, product_number_sku } = ctx.args
+  const { base_store } = ctx.args
   
     return {
       operation: "BatchGetItem",
       tables: {
-        posts: util.dynamodb.toMapValues({ base_store, product_number_sku: product_number_sku[0] })
+        posts: [util.dynamodb.toMapValues({ base_store })]
       }
     };
 }
