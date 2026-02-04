b397ad4c88dad81dd577aa9284bc9180d338fe33
Josephat <jogwayi@users.noreply.github.com>
2026-02-04 21:07:45 +0300

    sample backend post

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,19 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { sendEmail } from "./functions/sendEmail/resource";
 
 export const backend = defineBackend({
   auth,
+  sendEmail,
   data,
+});
+backend.sendEmail.resources.lambda.addFunctionUrl({
+  authType: 'NONE',
+  cors: {
+    allowCredentials: false,
+    allowedHeaders: ['*'],
+    allowedMethods: ['POST'],
+    allowedOrigins: ['*'],
+  },
 });
